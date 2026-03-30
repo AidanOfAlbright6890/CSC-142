d8d699211c3a0259f0683784d9c254bb8bee8288
Aidan Smith <aidan.smith001@albright.edu>
2026-03-29 22:34:14 -0400

    Add credits for source material in Item.py
    Added credits for Irv Kalb's code and ideas.

--- Inheritance/Item.py
+++ Inheritance/Item.py
@@ -1,3 +1,6 @@
+# Credit goes to Irv Kalb's code in chapter 10 of the book Object Oriented Python
+# Credit also goes to Irv Kalb's idea of Encapsulation in chapter 9 from Object Oriented Python that was used as part of this code
+
 from abc import ABC, abstractmethod
 
 
