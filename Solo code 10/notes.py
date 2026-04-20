3d8e84dae8db66c7dd318d43af8c24986beed327
Aidan Smith <aidan.smith001@albright.edu>
2026-04-19 23:37:03 -0400

    Delete Solo code 10/notes.py

--- Solo code 10/notes.py
+++
@@ -1,7 +0,0 @@
-#time.time() returns the number of seconds that have passed since a certain time
-# you can figure out how much time has passed with time.time(), then create another time.time() to subract the numbers
-# time.time() - timeStarted will give elapsed time if timeStarted is set to time.time()
-#TIMER_LENGTH is number of times desired to go
-# There can be a boolean set to true for timer running and set to false for timer ending to let the code know that time is happening
-# Time is important for animations to run, because animation is an image that changes over time, so animation needs timers to change
-#Class variables are defined by init
